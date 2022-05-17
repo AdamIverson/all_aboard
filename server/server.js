5ae09e73b9a678f5f2f88f579bb10f7bc7392416
Adam Iverson <aciverso@gmail.com>
2022-05-16 19:51:27 -0500

    backend not quite connected to front

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use('/api/test', testRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 /** Listen * */
 app.listen(PORT, () => {
--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,2 +1,32 @@
-import React from "react";
-import axios
+import React, { useState } from "react";
+import axios from "axios";
+
+function Test() {
+  const [test, setTest] = useState([]);
+
+  function call() {
+    axios({
+      method: "GET",
+      url: "/api/test",
+    })
+      .then((response) => {
+        setTest(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div>
+      <button onClick={call}>Button</button>
+      <ul>
+        {test.map((item) => {
+          return (<li>item.testData</li>);
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Test;
